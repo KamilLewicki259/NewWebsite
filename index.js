24beea4c49cc5b9472a50ff5c0b26ad7fb120c4f
Envision <59775045+KamilLewicki259@users.noreply.github.com>
2024-08-18 17:46:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("myButton").onclick = function() {
+	alert("This is a JavaScript powered alert.");
+};
+document.getElementById("moreInfoButton1").onclick = function() {
+	alert("More info provided.");
+};
+document.getElementById("moreInfoButton3").onclick = function() {
+	alert("More info provided.");
+};
+document.getElementById("moreInfoButton2").onclick = function() {
+	alert("More info provided.");
+};
